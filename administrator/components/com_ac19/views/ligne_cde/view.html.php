133cbeb1eb291ab96beb779f7e3ee75b0c5134d4
Wassim MSALMI <wassimgtarp@gmail.com>
2020-12-07 18:03:03 +0100

    vue de detail

---
+++ administrator/components/com_ac19/views/ligne_cde/view.html.php
@@ -0,0 +1,55 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+ 
+class Ac19ViewLigne_cde extends JViewLegacy
+{
+	protected $form;
+	protected $item;
+	protected $state;
+	
+	public function display($tpl = null) 
+	{
+		$this->form		= $this->get('Form');
+		$this->item		= $this->get('Item');
+		$this->state	= $this->get('State');
+
+		if (count($errors = $this->get('Errors'))) 
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+			return false;
+		}
+
+		$this->addToolBar();
+		parent::display($tpl);
+	}
+
+	protected function addToolBar() 
+	{			
+		$input = JFactory::getApplication()->input;
+		$input->set('hidemainmenu', true);
+
+		$user		= JFactory::getUser();
+		$userId		= $user->get('id');
+		// $checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $userId);
+	
+		// affiche le titre de la page
+		$isNew = ($this->item->id == 0);
+		JToolBarHelper::title(JText::_('COM_AC19_LIGNE_CDE')." : ".($isNew ? JText::_('COM_AC19_NEW'): JText::_('COM_AC19_MODIF')), 'address');
+
+		if ($isNew)
+		{
+			JToolbarHelper::apply('ligne_cde.apply');
+			JToolbarHelper::save('ligne_cde.save');
+			JToolbarHelper::save2new('ligne_cde.save2new');
+		}
+		else
+		{
+			// if (!$checkedOut)
+			// {
+				JToolbarHelper::apply('ligne_cde.apply');
+				JToolbarHelper::save('ligne_cde.save');
+			// }
+		}
+		JToolBarHelper::cancel('ligne_cde.cancel', $isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE');
+	}
+}
