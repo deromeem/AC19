b8f7a320443780fefddc50ba381a901ce51e6c0c
Emmanuel DEROME <emmanuel.derome@gmail.com>
2021-03-12 17:00:09 +0100

    MAJ Joomla 3.9.25

--- administrator/logs/1.error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2020-10-27 13:59:35 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2020-10-27T13:59:35+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
