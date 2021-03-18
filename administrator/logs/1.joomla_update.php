dd552712663079ba734c4df0bfb0e7e83ef05cf9
Jérémy ROLLO <jrollo@louisarmand.local>
2021-03-18 16:05:25 +0100

    commande vue detail

---
+++ administrator/logs/1.joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-11-25 14:10:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-11-25T14:10:37+00:00	INFO ::1	update	Mise à jour lancée par l'utilisateur Super Utilisateur (669). L'ancienne version est 3.9.22.
+2020-11-25T14:10:39+00:00	INFO ::1	update	Téléchargement du fichier de mise à jour depuis https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.23-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20201125%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20201125T141024Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=d20fee0798dd6fbed853e1a3ba10f52fb9a67e4bd3802f0e5228073703f1d752.
+2020-11-25T14:10:42+00:00	INFO ::1	update	Fichier Joomla_3.9.23-Stable-Update_Package.zip téléchargé.
+2020-11-25T14:10:43+00:00	INFO ::1	update	Lancement de l'installation de la nouvelle version.
+2020-11-25T14:12:28+00:00	INFO ::1	update	Conclusion de l'installation.
+2020-11-25T14:12:28+00:00	INFO ::1	update	Effacement des fichiers et dossiers à supprimer.
+2020-11-25T14:12:29+00:00	INFO ::1	update	Mise en ordre après installation.
+2020-11-25T14:12:29+00:00	INFO ::1	update	La mise à jour vers la version 3.9.23 est achevée.
