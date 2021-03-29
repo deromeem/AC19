09d09644670632f2d8883cc28a108f943d052d9a
Sakina ANVARALY <anvaralysakina@yahoo.fr>
2021-03-29 17:04:52 +0200

    Modification MesMessages

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-18 14:54:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-18T14:54:36+00:00	INFO ::1	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
---
+++ components/com_ac19/views/messages/tmpl/default.php
@@ -0,0 +1,24 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+JHtml::_('behavior.framework'); 				// javascript Joomla object for grid.sort !
+
+$user = JFactory::getUser();               		// gets current user object
+$isAdmin = (in_array('2', $user->groups));		// sets flag when user group is '2' that is 'Enregistré' 
+?>
+
+<?php if (!$isAdmin) : ?>
+	<?php echo JError::raiseWarning( 100, JText::_('COM_AC19_RESTRICTED_ACCESS') ); ?>
+<?php else : ?>
+
+	<h2><?php echo JText::_('COM_AC19_OPTIONS')." : ".JText::_('COM_AC19_MESSAGES')." - "; ?>
+		<a href="<?php echo JRoute::_('index.php?option=com_ac19&view=messages'); ?>">
+			<?php echo JText::_('COM_AC19_MESSAGES'); ?>
+		</a>
+	</h2>
+
+	<?php echo $this->loadTemplate('items'); ?>
+
+	<?php echo $this->pagination->getListFooter(); ?>
+
+<?php endif; ?>
