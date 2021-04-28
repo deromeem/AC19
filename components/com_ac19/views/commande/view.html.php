380f861af695ae4fe4b0be33898a950631affba0
JeremyRollo <66129356+JeremyRollo@users.noreply.github.com>
2021-04-28 20:29:06 +0200

    UPDATE_BDD_AC19_&_COMMANDE
    Ajout de données sur la base de AC19 et développement de la vue de détail commande avec l'affichage des produits d'une commande.

--- components/com_ac19/views/commande/view.html.php
+++ components/com_ac19/views/commande/view.html.php
@@ -4,12 +4,14 @@ defined('_JEXEC') or die('Restricted access');
 class Ac19ViewCommande extends JViewLegacy
 {
 	protected $item;
+	protected $pages;
 	
 	function display($tpl = null) 
 	{
 		// initialise les variables
 		$this->item = $this->get('Item');
-
+		$this->pages = $this->get('Product');
+		// var_dump($this->pages);
 		// affiche les erreurs
 		if (count($errors = $this->get('Errors'))) {
 			JError::raiseWarning(500, implode("\n", $errors));
